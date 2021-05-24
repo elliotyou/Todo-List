c8f67eeac5a3c9d238e5c7551b2359331cce70b5
elliotyou <m9402311@mail.ntust.edu.tw>
2021-05-24 09:32:02 +0800

    heroku init

--- app.js
+++ app.js
@@ -1,78 +1,25 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+const routes = require('./routes')
+require('./config/mongoose')
+
 const app = express()
-const port = 3000
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-const Todo = require('./models/todo')
+const PORT = process.env.PORT || 3000
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
 app.use(bodyParser.urlencoded({ extended: true }))
 
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+app.use(methodOverride('_method'))
 
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+app.use('/', routes)
 
-app.get('/', (req, res) => {
-  Todo.find()
-    .lean()
-    .sort({ _id: 'asc' })
-    .then(todos => res.render('index', { todos }))
-    .catch(error => console.error(error))
-})
+console.log('in app.js, after use routes')
 
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then(todo => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.post('/todos', (req, res) => {
-  const name = req.body.name
-  Todo.create({ name })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then(todo => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
-app.post('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(erorr => console.log(error))
-})
-
-app.post('/todos/:id/delete', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-app.listen(port, () => {
-  console.log(`app is running on http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`app is running on http://localhost:${PORT}`)
 })
