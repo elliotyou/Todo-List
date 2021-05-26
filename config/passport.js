bed9ff3ab64cdc1d3c0104b99f4832efa6bcd95d
elliotyou <m9402311@mail.ntust.edu.tw>
2021-05-26 23:46:08 +0800

    add facebook

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
+const bcrypt = require('bcryptjs')
 
 const User = require('../models/user')
 
@@ -13,13 +15,42 @@ module.exports = app => {
         if (!user) {
           return done(null, false, { message: 'That email is not registered!' })
         }
-        if (user.password !== password) {
-          return done(null, false, { message: 'Email or Password incorrect.' })
-        }
-        return done(null, user)
+        return bcrypt.compare(password, user.password).then(isMatch => {
+          if (!isMatch) {
+            return done(null, false, { message: 'Email or Password incorrect.' })
+          }
+          return done(null, user)
+        })
       })
       .catch(err => done(err, false))
   }))
+
+  passport.use(new FacebookStrategy({
+    clientID: 'aaaaaaa',
+    clientSecret: 'bbbbbbbbbbbbbbbbb',
+    callbackURL: "http://localhost:3000/auth/facebook/callback",
+    profileFields: ['email', 'displayName']
+  },
+    (accessToken, refreshToken, profile, done) => {
+      const { name, email } = profile._json
+      User.findOne({ email })
+        .then(user => {
+          if (user) return done(null, user)
+
+          const randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(randomPassword, salt))
+            .then(hash => User.create({
+              name,
+              email,
+              password: hash
+            }))
+            .then(user => done(null, user))
+            .catch(err => done(err, false))
+        })
+    }
+  ));
 
   passport.serializeUser((user, done) => {
     done(null, user.id)
